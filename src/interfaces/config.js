d211926ffa9be933fcc2070a6e0a660466c10e64
Israel Santana <73674173+SantanaRael@users.noreply.github.com>
2024-01-25 15:44:00 -0300

    Update config.js

--- src/interfaces/config.js
+++ src/interfaces/config.js
@@ -1,7 +1,7 @@
 const links = {
     linkedin: 'https://www.linkedin.com/in/israel-sant',
     github: 'https://github.com/SantanaRael',
-    curriculo: '/icons/Currículo - Israel Santana da Silva.pdf',
+    curriculo: 'https://raw.githubusercontent.com/SantanaRael/my-links/main/public/icons/Curr%C3%ADculo%20-%20Israel%20Santana%20da%20Silva.pdf',
     email: 'mailto:israel100santana@gmail.com',
   };
   
