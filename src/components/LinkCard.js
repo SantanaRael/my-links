6a957543661a70c279c1b9f0dd4dab003f8fd579
Israel Santana <israel100santana@gmail.com>
2024-01-26 11:57:04 -0300

    ajuste na largura dos cards

--- src/components/LinkCard.js
+++ src/components/LinkCard.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import './LinkCard.css'; 
+import './LinkCard.css';
 
 function LinkCard({ link }) {
+  const redirectToLink = () => {
+    window.open(link.url, '_blank', 'noopener noreferrer');
+  };
+
   return (
-    <div className="link-card">
-        <img src={process.env.PUBLIC_URL + link.icon}/>
-        <a href={link.url} target="_blank" rel="noopener noreferrer">
+    <div className="link-card" onClick={redirectToLink}>
+      <img src={process.env.PUBLIC_URL + link.icon} alt={link.name} />
+      <a href={link.url} target="_blank" rel="noopener noreferrer">
         {link.name}
-        </a>
+      </a>
     </div>
   );
-}    
-
+}
 
 export default LinkCard;
+
